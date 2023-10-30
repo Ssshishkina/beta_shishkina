66123794e478643af6c7e764eb25c90782e5131c
nastya shishkina <ana.koze@yandex.ru>
2023-10-30 16:54:26 +0400

    добавлен докстринг

--- src/widget.py
+++ src/widget.py
@@ -3,7 +3,13 @@ from masks import card_number_mask, invoice_number_mask
 
 def type_of_cards(type_and_number): str | list[str], account_and_number) -> str:
     """Принимает на вход строку информацией тип карты/счета и номер карты/счета.
-    Возвращает эту строку с замаскированным номером карты/счета"""
+       Возвращает эту строку с замаскированным номером карты/счета"""
     split_list = type_and_number.split()
     split_account = account_and_number.split()
+    only_account = invoice_number_mask(split_account[-1])
+    only_number_account = split_account[:-1]
+    join_oneline = ''.join(only_number_account)
+    if len(split_list) == 3:
+        only_number = card_number_mask(split_list[-1])
+        only_type = split_list[:2]
 
